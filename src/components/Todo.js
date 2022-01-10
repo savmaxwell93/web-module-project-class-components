e4338b45473fd2df07a15a8ec162ae4931ca4cbb
Savannah <savmaxwell@gmail.com>
2022-01-10 13:22:42 -0700

    built out DOM and skeleton components

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    render() {
+        return(
+            <ul>
+                <li></li>
+            </ul>
+        )
+    }
+}
+
+export default Todo;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    render() {
+        return(
+            <div>
+            <p>New Task Form</p>
+            <form>
+              <input type='text' placeholder='Add a new task' />
+              <button>Add Task</button>
+            </form>
+          </div>
+        )
+    }
+}
+
+export default TodoForm;
